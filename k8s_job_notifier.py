eb96e53e6309dd05e662ee2356838295a8ab08c8
zanderhavgaard <zander@havgaard.dk>
2024-08-08 17:45:22 +0200

    initial logic flow

--- k8s_job_notifier.py
+++ k8s_job_notifier.py
@@ -1,3 +1,4 @@
+from time import sleep
 from kubernetes import client, config
 
 # load local kubeconfig
@@ -10,6 +11,41 @@ k8s_client = client.BatchV1Api()
 
 namespace = "assets"
 
-cron_jobs = k8s_client.list_namespaced_cron_job(namespace=namespace)
+finished_jobs = {}
 
-print(cron_jobs)
+
+def send_slack_message():
+    print("send slack message here")
+
+
+send_message_on_job_success = False
+loop_sleep_seconds = 5
+
+while True:
+    jobs = k8s_client.list_namespaced_job(namespace=namespace)
+
+    for job in jobs.items:
+        job_name = job.metadata.name
+        job_creation_time = str(int(job.metadata.creation_timestamp.timestamp()))
+        job_hash = f"{job_name}-{job_creation_time}"
+        job_status = job.status
+
+        # if job is still running, continue
+        if job_status.active:
+            continue
+
+        # if we have seen this job before, continue
+        if job_hash in finished_jobs:
+            continue
+
+        if job_status.failed:
+            send_slack_message()
+
+        if job_status.succeeded:
+            if send_message_on_job_success:
+                send_slack_message()
+
+        # add job to dict, so we don't send message twice
+        finished_jobs[job_hash] = True
+
+    sleep(loop_sleep_seconds)
