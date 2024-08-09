9638f2a85933db1b684f683ca58cc0b3365c3c35
zanderhavgaard <zander@havgaard.dk>
2024-08-09 22:39:45 +0200

    fix more linter errors

--- kubernetes_job_notifier.py
+++ kubernetes_job_notifier.py
@@ -2,24 +2,46 @@
 Send slack messages when Kubernetes Jobs finish.
 """
 
+from enum import Enum
+from dataclasses import dataclass
 from time import sleep, time
 from datetime import datetime
-from kubernetes import config
-from kubernetes.client import BatchV1Api
+from kubernetes import client, config
 from loguru import logger
 from dynaconf import Dynaconf
 from slack_sdk import WebhookClient
+
+
+class JobState(Enum):
+    """
+    Represent possible states of job, makes it convenient to switch on
+    """
+
+    ACTIVE = "active"
+    FAILED = "failed"
+    SUCCEEDED = "succeeded"
+
+
+@dataclass
+class ParsedKubernetesMetadata:
+    """
+    Hold just the relevant job metadata for sending the slack message
+    """
+
+    name: str
+    state: JobState
+    creation_timestamp: datetime
+    name_hash: str
 
 
 def _send_slack_message_job_failed(
     webhook_url: str,
     slack_users_to_notify: list[str],
     namespace: str,
-    job_name: str,
-    job_creation_timestamp: datetime,
+    job_metadata: ParsedKubernetesMetadata,
 ) -> None:
     logger.info(
-        f"Sending for job: {job_name}, created at {job_creation_timestamp} in namespace {namespace} ..."
+        f"Sending for job: {job_metadata.name}, created at {str(job_metadata.creation_timestamp)} in namespace {namespace} ..."
     )
 
     # create the message blocks
@@ -38,9 +60,9 @@ def _send_slack_message_job_failed(
             "type": "section",
             "text": {
                 "type": "mrkdwn",
-                "text": f"Job Name: `{job_name}`\n"
+                "text": f"Job Name: `{job_metadata.name}`\n"
                 f"Namespace: `{namespace}`\n"
-                f"Creation timestamp `{str(job_creation_timestamp)}`",
+                f"Creation timestamp `{str(job_metadata.creation_timestamp)}`",
             },
         },
     ]
@@ -59,7 +81,7 @@ def _send_slack_message_job_failed(
     webhook_client = WebhookClient(webhook_url)
 
     logger.info("Sending slack message ...")
-    response = webhook_client.send(blocks=message_blocks)
+    response = webhook_client.send(blocks=message_blocks)  # type: ignore
 
     if response:
         logger.info(
@@ -67,17 +89,16 @@ def _send_slack_message_job_failed(
         )
 
     if response.status_code == 200 and response.body == "ok":
-        logger.info(f"Successfully sent slack message for job: {job_name}.")
+        logger.info(f"Successfully sent slack message for job: {job_metadata.name}.")
 
 
 def _send_slack_message_job_succeeded(
     webhook_url: str,
     namespace: str,
-    job_name: str,
-    job_creation_timestamp: datetime,
+    job_metadata: ParsedKubernetesMetadata,
 ) -> None:
     logger.info(
-        f"Sending for job: {job_name}, created at {job_creation_timestamp} in namespace {namespace} ..."
+        f"Sending for job: {job_metadata.name}, created at {job_metadata.creation_timestamp} in namespace {namespace} ..."
     )
 
     # create the message blocks
@@ -96,29 +117,18 @@ def _send_slack_message_job_succeeded(
             "type": "section",
             "text": {
                 "type": "mrkdwn",
-                "text": f"Job Name: `{job_name}`\n"
+                "text": f"Job Name: `{job_metadata.name}`\n"
                 f"Namespace: `{namespace}`\n"
-                f"Creation timestamp `{str(job_creation_timestamp)}`",
+                f"Creation timestamp `{str(job_metadata.creation_timestamp)}`",
             },
         },
     ]
-    # TODO: consider if this is useful
-    # if slack_users_to_notify:
-    #     message_blocks.append(
-    #         {
-    #             "type": "section",
-    #             "text": {
-    #                 "type": "mrkdwn",
-    #                 "text": f"Notifying: {slack_users_to_notify}",
-    #             },
-    #         },
-    #     )
 
     logger.info("Creating slack API client ...")
     webhook_client = WebhookClient(webhook_url)
 
     logger.info("Sending slack message ...")
-    response = webhook_client.send(blocks=message_blocks)
+    response = webhook_client.send(blocks=message_blocks)  # type: ignore
 
     if response:
         logger.info(
@@ -126,17 +136,17 @@ def _send_slack_message_job_succeeded(
         )
 
     if response.status_code == 200 and response.body == "ok":
-        logger.info(f"Successfully sent slack message for job: {job_name}.")
+        logger.info(f"Successfully sent slack message for job: {job_metadata.name}.")
 
 
-def _create_kubernetes_api_client(use_kube_config: bool = False) -> BatchV1Api:
+def _create_kubernetes_api_client(use_kube_config: bool = False) -> client.BatchV1Api:
     if use_kube_config:
         # load local kubeconfig
-        config.load_kube_config()
+        config.load_kube_config()  # type: ignore
     else:
         # or when running in a cluster, use ie. a ServiceAccount.
-        config.load_incluster_config()
-    kubernetes_client = BatchV1Api()
+        config.load_incluster_config()  # type: ignore
+    kubernetes_client = client.BatchV1Api()
 
     return kubernetes_client
 
@@ -151,15 +161,61 @@ def _load_settings() -> Dynaconf:
 
 
 def _delete_old_job_hashes(
-    expiration_time_seconds: int, seen_jobs: dict[str, int]
+    expiration_time_seconds: int, seen_jobs: dict[str, float]
 ) -> None:
     hashes_to_delete = []
     current_time = time()
     for job_hash, timestamp in seen_jobs.items():
-        if current_time - timestamp > expiration_time_seconds:
+        if current_time - timestamp > int(expiration_time_seconds):
             hashes_to_delete.append(job_hash)
     for job_hash in hashes_to_delete:
         del seen_jobs[job_hash]
+
+
+def _parse_kubernetes_jobs(
+    job_list: client.V1JobList,
+) -> list[ParsedKubernetesMetadata]:
+    parsed_jobs = []
+
+    for job in job_list.items:
+        # assert shenanigans to satisfy mypy
+
+        # extract metadata
+        assert job
+        job_metadata = job.metadata
+        assert job_metadata
+        assert job_metadata.name
+        job_name = job_metadata.name
+        job_creation_timestamp = job_metadata.creation_timestamp
+        assert job_creation_timestamp
+        job_creation_timestamp_str = str(int(job_creation_timestamp.timestamp()))
+
+        # extract job status
+        assert job.status
+        job_status = job.status
+        # job status is an int
+        if job_status.active:
+            job_state = JobState.ACTIVE
+        elif job_status.succeeded:
+            job_state = JobState.SUCCEEDED
+        elif job_status.failed:
+            job_state = JobState.FAILED
+        else:
+            raise RuntimeError(f"Could not determine job status, for job: {job.status}")
+
+        # create a unique hashable value for the job
+        job_hash = f"{job_name}-{job_creation_timestamp_str}"
+
+        parsed_job = ParsedKubernetesMetadata(
+            name=job_name,
+            state=job_state,
+            creation_timestamp=job_creation_timestamp,
+            name_hash=job_hash,
+        )
+
+        parsed_jobs.append(parsed_job)
+
+    return parsed_jobs
 
 
 def entrypoint() -> None:
@@ -200,48 +256,56 @@ def entrypoint() -> None:
 
     logger.info("Starting main loop ...")
     while True:
-        jobs = kubernetes_client.list_namespaced_job(namespace=namespace)
+        job_list: client.V1JobList = kubernetes_client.list_namespaced_job(
+            namespace=namespace
+        )
 
-        for job in jobs.items:
-            job_name = job.metadata.name
-            job_creation_time = job.metadata.creation_timestamp
-            job_creation_timestamp_str = str(int(job_creation_time.timestamp()))
-            job_hash = f"{job_name}-{job_creation_timestamp_str}"
-            job_status = job.status
+        parsed_jobs = _parse_kubernetes_jobs(job_list=job_list)
 
-            # if job is still running, continue
-            if job_status.active:
-                continue
+        for job in parsed_jobs:
 
-            # if we have seen this job before, continue
-            if job_hash in seen_jobs:
-                continue
+            match job.state:
+                case JobState.ACTIVE:
+                    # if job is still running, continue
+                    continue
+                case JobState.SUCCEEDED:
+                    if job.name_hash in seen_jobs:
+                        # if we have seen this job before, continue
+                        continue
+                    if send_message_on_job_success:
+                        logger.info(
+                            f"Saw completed job: {job.name}, sending slack message ..."
+                        )
+                        _send_slack_message_job_succeeded(
+                            webhook_url=slack_webhook_url,
+                            namespace=namespace,
+                            job_metadata=job,
+                        )
+                        # add job to dict, so we don't send message twice
+                        logger.info(f"adding hash: {job.name_hash}")
+                        seen_jobs[job.name_hash] = time()
+                case JobState.FAILED:
+                    if job.name_hash in seen_jobs:
+                        # if we have seen this job before, continue
+                        continue
 
-            if job_status.failed:
-                logger.info(f"Saw failed job: {job_name}, sending slack message ...")
-                _send_slack_message_job_failed(
-                    webhook_url=slack_webhook_url,
-                    slack_users_to_notify=slack_users_to_notify,
-                    job_name=job_name,
-                    job_creation_timestamp=job_creation_time,
-                    namespace=namespace,
-                )
-
-            if job_status.succeeded:
-                if send_message_on_job_success:
                     logger.info(
-                        f"Saw completed job: {job_name}, sending slack message ..."
+                        f"Saw failed job: {job.name}, sending slack message ..."
                     )
-                    _send_slack_message_job_succeeded(
+                    _send_slack_message_job_failed(
                         webhook_url=slack_webhook_url,
-                        job_name=job_name,
-                        job_creation_timestamp=job_creation_time,
+                        slack_users_to_notify=slack_users_to_notify,
                         namespace=namespace,
+                        job_metadata=job,
                     )
+                    # add job to dict, so we don't send message twice
+                    logger.info(f"adding hash: {job.name_hash}")
+                    seen_jobs[job.name_hash] = time()
 
-            # add job to dict, so we don't send message twice
-            logger.info(f"adding hash: {job_hash}")
-            seen_jobs[job_hash] = time()
+        print("---")
+        print("seen jobs")
+        print(seen_jobs)
+        print("---")
 
         # delete old job hashes
         expiration_time_seconds = 30
